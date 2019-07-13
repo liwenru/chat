532155a43d03aa3196f6f12fb39e6294cb0656b2
liwenru <1443759307@qq.com>
2019-07-13 16:20:54 +0800

    文章列表完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,16 +3,19 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
+  
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
+    })
+  },
+  onTapJump:function(){
+    wx.redirectTo({
+      url: '../post/post',
+      success:function(){
+        console.log("jump success")
+      }
     })
   },
   onLoad: function () {
